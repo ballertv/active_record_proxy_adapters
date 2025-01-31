f2e9769a1f6fa613cb78f2a54a7a9220de1eca94
Bo Kunesh <bo@baller.tv>
2025-01-31 00:36:03 -0800

    Revert "Drop support for Rails 6.1.x"
    This reverts commit 8ba60d35ae68f34970dcbcc8034e76bc27a04bbe.

--- lib/active_record_proxy_adapters/active_record_context.rb
+++ lib/active_record_proxy_adapters/active_record_context.rb
@@ -4,7 +4,7 @@ module ActiveRecordProxyAdapters
   # Collection of helpers to handle common active record methods that are defined in different places in different
   # versions of rails.
   class ActiveRecordContext
-    delegate :reading_role, :reading_role=, :writing_role, :writing_role=, to: :ActiveRecord
+    delegate :reading_role, :reading_role=, :writing_role, :writing_role=, to: :role_context
     delegate :legacy_connection_handling, :legacy_connection_handling=, to: :connection_handling_context
     delegate :version, to: :ActiveRecord, prefix: :active_record
 
@@ -22,15 +22,37 @@ module ActiveRecordProxyAdapters
       end
     end
 
+    def reading_role
+      role_context.reading_role
+    end
+
+    def writing_role
+      role_context.writing_role
+    end
+
     def connection_class_for(connection)
-      connection.connection_class || ActiveRecord::Base
+      klass = active_record_v6_1? ? connection.connection_klass : connection.connection_class
+
+      klass || ActiveRecord::Base
     end
 
     def connection_handling_context
       # This config option has been removed in Rails 7.1+
       return NullConnectionHandlingContext.new if active_record_v7_1_or_greater?
 
-      ActiveRecord
+      global_configuration_context
+    end
+
+    def role_context
+      global_configuration_context
+    end
+
+    def global_configuration_context
+      active_record_v6_1? ? ActiveRecord::Base : ActiveRecord
+    end
+
+    def active_record_v6_1?
+      active_record_version >= Gem::Version.new("6.1") && active_record_version < Gem::Version.new("7.0")
     end
 
     def active_record_v7_1_or_greater?
